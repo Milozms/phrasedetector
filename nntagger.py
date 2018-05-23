fb4c4f49a72881b68d2b3ddb0fce6a0fd7526ad8
zms <zhangmaosen@pku.edu.cn>
2018-05-23 14:46:17 +0800

    5.23 use "crf_decode" in model

--- nntagger.py
+++ nntagger.py
@@ -5,6 +5,7 @@ import logging
 from tqdm import tqdm
 import os
 from tensorflow.contrib import crf
+import math
 os.environ["CUDA_VISIBLE_DEVICES"] = '1'
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
@@ -69,23 +70,32 @@ def readfile_pos_ner(filename, word_dict):
 	sentences_ner = []
 	seq_len = []
 	dataLines = f.readlines()
+	dset = []
+	instance = {}
 	for i in range(len(dataLines)):
 		line = dataLines[i]
 		tokens = line.split()
 		if i % 4 == 0:
 			tag = [tagMap[token] for token in tokens] + [0]*(maxlen-len(tokens))
-			sentences_tags.append(tag)
+			# sentences_tags.append(tag)
+			instance['tag'] = tag
 		elif i % 4 == 1:
 			words = [word_dict[token] for token in tokens] + [0]*(maxlen-len(tokens))
-			sentences.append(words)
+			# sentences.append(words)
+			instance['words'] = words
 		elif i % 4 == 2:
 			poss = [posMap[token] for token in tokens] + [0]*(maxlen - len(tokens))
-			sentences_pos.append(poss)
+			# sentences_pos.append(poss)
+			instance['pos'] = poss
 		else:
 			ners = [nerMap[token] for token in tokens] + [0]*(maxlen - len(tokens))
-			sentences_ner.append(ners)
-			seq_len.append(len(tokens))
-	return sentences, sentences_tags, seq_len, sentences_pos, sentences_ner
+			# sentences_ner.append(ners)
+			instance['ner'] = ners
+			# seq_len.append(len(tokens))
+			instance['len'] = len(tokens)
+			dset.append(instance)
+			instance = {}
+	return dset
 
 
 def stacked_lstm(inputs, hidden, num_layers, seq_len, batch, keep_prob=1.0, concat_layers=True, scope="StackedLSTM"):
@@ -192,12 +202,12 @@ class Model(object):
 		output = tf.concat([output[0], output[1]], axis=2)
 		logits = tf.layers.dense(output, len(tagMap))
 
-		# logits = tf.nn.softmax(logits, dim=2)
+		logits = tf.nn.softmax(logits, dim=2)
 
-		log_likelihood, transition_params = \
+		log_likelihood, self.transition_params = \
 			crf.crf_log_likelihood(logits, self.y_inputs, self.seq_len)
 		viterbi_sequence, viterbi_score = \
-			crf.crf_decode(logits, transition_params, self.seq_len)
+			crf.crf_decode(logits, self.transition_params, self.seq_len)
 		self.cost = tf.reduce_mean(-log_likelihood)
 
 
@@ -224,88 +234,47 @@ class Model(object):
 											 global_step=tf.contrib.framework.get_or_create_global_step())
 		return
 
-	def build_model_0(self):
-		self.X_inputs = tf.placeholder(tf.int32, [None, maxlen], name='X_input')
-		self.y_inputs = tf.placeholder(tf.int32, [None, maxlen], name='y_input')
-		self.pos = tf.placeholder(tf.int32, [None, maxlen], name='pos')
-		self.ner = tf.placeholder(tf.int32, [None, maxlen], name='ner')
-		self.seq_len = tf.placeholder(tf.int32, [None], name="seq_len")
+	def valid(self, test_dset):
+		test_sentences = [instance['words'] for instance in test_dset]
+		test_seq_len = [instance['len'] for instance in test_dset]
+		test_pos = [instance['pos'] for instance in test_dset]
+		test_ner = [instance['ner'] for instance in test_dset]
 
-		with tf.device("/cpu:0"):
-			word_emb = tf.nn.embedding_lookup(self.embedding, self.X_inputs)
-			word_emb = tf.cast(word_emb, dtype=tf.float32)
-			pos_embedding = tf.get_variable("pos_embedding",
-											shape=[len(posSet), self.posdim],
-											trainable=True,
-											dtype=tf.float32,
-											initializer=tf.random_normal_initializer())
-			ner_embedding = tf.get_variable("ner_embedding",
-											shape=[len(nerSet), self.nerdim],
-											trainable=True,
-											dtype=tf.float32,
-											initializer=tf.random_normal_initializer())
-			# pos = tf.one_hot(self.pos, len(posSet), axis=-1, dtype=tf.float32)
-			# ner = tf.one_hot(self.ner, len(nerSet), axis=-1, dtype=tf.float32)
-			pos = tf.nn.embedding_lookup(pos_embedding, self.pos)
-			ner = tf.nn.embedding_lookup(ner_embedding, self.ner)
-			inputs = tf.concat([word_emb, pos, ner], axis=2)
+		ans = sess.run([self.y_ans],
+							feed_dict={self.X_inputs: test_sentences,
+									   self.seq_len: test_seq_len,
+									   self.pos: test_pos,
+									   self.ner: test_ner})
+		sent_acc_count = 0.0
+		word_acc_count = 0.0
+		word_count = 0.0
+		test_tags = [instance['tag'] for instance in test_dset]
 
-		m_cell_fw = []
-		m_cell_bw = []
-		for i in range(self.num_layers):
-			cell_fw = tf.nn.rnn_cell.BasicLSTMCell(num_units=self.hidden_size)
-			cell_fw = tf.nn.rnn_cell.DropoutWrapper(cell=cell_fw, input_keep_prob=self.keep_prob, output_keep_prob=self.keep_prob)
-			cell_bw = tf.nn.rnn_cell.BasicLSTMCell(num_units=self.hidden_size)
-			cell_bw = tf.nn.rnn_cell.DropoutWrapper(cell=cell_bw, input_keep_prob=self.keep_prob, output_keep_prob=self.keep_prob)
-			m_cell_fw.append(cell_fw)
-			m_cell_bw.append(cell_bw)
-
-		batch = tf.shape(self.X_inputs)[0]
-		m_cell_fw = tf.nn.rnn_cell.MultiRNNCell(m_cell_fw, state_is_tuple=True)
-		m_cell_bw = tf.nn.rnn_cell.MultiRNNCell(m_cell_bw, state_is_tuple=True)
-		init_state_fw = m_cell_fw.zero_state(batch, dtype=tf.float32)
-		init_state_bw = m_cell_bw.zero_state(batch, dtype=tf.float32)
-		output, state = tf.nn.bidirectional_dynamic_rnn(cell_fw=m_cell_fw, cell_bw=m_cell_bw, inputs=inputs,
-														sequence_length=self.seq_len, scope='bi_lstm', dtype=tf.float32,
-														initial_state_fw=init_state_fw, initial_state_bw=init_state_bw)
-		output = tf.concat([output[0], output[1]], axis=2)
-		logits = tf.layers.dense(output, len(tagMap))
-
-		# logits = tf.nn.softmax(logits, dim=2)
-
-		log_likelihood, transition_params = \
-			crf.crf_log_likelihood(logits, self.y_inputs, self.seq_len)
-		viterbi_sequence, viterbi_score = \
-			crf.crf_decode(logits, transition_params, self.seq_len)
-		self.cost = tf.reduce_mean(-log_likelihood)
-
-
-		# loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.y_inputs, logits=logits)
-		# [batch, time_step]
-		seq_mask = tf.sequence_mask(self.seq_len, maxlen, tf.float32)
-		# self.cost = tf.reduce_mean(seq_mask * loss)
-
-		# self.y_ans = tf.cast(tf.argmax(logits, 2), tf.int32)
-		self.y_ans = viterbi_sequence
-		correct_prediction = tf.equal(self.y_ans, self.y_inputs)  # [batch, time_step]
-		correct_cnt = tf.reduce_sum(seq_mask * tf.cast(correct_prediction, tf.float32), axis=1)
-		# [batch]
-		accuracy = correct_cnt / tf.cast(self.seq_len, tf.float32)
-		self.accuracy = tf.reduce_mean(accuracy)
-
-		tvars = tf.trainable_variables()
-		# 获取损失函数对于每个参数的梯度
-		grads, _ = tf.clip_by_global_norm(tf.gradients(self.cost, tvars), self.max_grad_norm)
-		# 优化器
-		optimizer = tf.train.AdamOptimizer(learning_rate=self.lr)
-		# 梯度下降计算
-		self.train_op = optimizer.apply_gradients(zip(grads, tvars),
-											 global_step=tf.contrib.framework.get_or_create_global_step())
-		return
+		for i in range(len(test_tags)):
+			tags = test_tags[i]
+			pred_true = True
+			for j in range(test_seq_len[i]):
+				word_count += 1.0
+				if ans[i][j] != tags[j]:
+					pred_true = False
+				else:
+					word_acc_count += 1.0
+			if pred_true:
+				sent_acc_count += 1.0
+		logging.info('word acc: %f, sentenceacc: %f' % (
+			word_acc_count / word_count, sent_acc_count / len(test_tags)))
+		sent_acc = sent_acc_count / len(test_tags)
+		return sent_acc
 
 	def train(self, train_dset, test_dset, max_epoch):
-		train_sentences, train_tags, train_seq_len, train_pos, train_ner = train_dset
-		test_sentences, test_tags, test_seq_len, test_pos, test_ner = test_dset
+		# train_sentences, train_tags, train_seq_len, train_pos, train_ner = train_dset
+		# test_sentences, test_tags, test_seq_len, test_pos, test_ner = test_dset
+		train_sentences = [instance['words'] for instance in train_dset]
+		train_tags = [instance['tag'] for instance in train_dset]
+		train_seq_len = [instance['len'] for instance in train_dset]
+		train_pos = [instance['pos'] for instance in train_dset]
+		train_ner = [instance['ner'] for instance in train_dset]
+
 
 		init_op = tf.initialize_all_variables()
 		sess.run(init_op)
@@ -319,29 +288,9 @@ class Model(object):
 																	   self.pos: train_pos,
 																	   self.ner: train_ner})
 			logging.info('epoch = %d, loss = %f' % (epoch, cost_val))
-			ans, acc = sess.run([self.y_ans, self.accuracy], feed_dict={self.X_inputs: test_sentences, self.y_inputs: test_tags,
-															  self.seq_len: test_seq_len,
-															  self.pos: test_pos,
-															  self.ner: test_ner})
-			sent_acc_count = 0.0
-			word_acc_count = 0.0
-			word_count = 0.0
-			for i in range(len(test_tags)):
-				tags = test_tags[i]
-				pred_true = True
-				for j in range(test_seq_len[i]):
-					word_count += 1.0
-					if ans[i][j] != tags[j]:
-						pred_true = False
-					else:
-						word_acc_count += 1.0
-				if pred_true:
-					sent_acc_count += 1.0
-			logging.info('test acc: %f, word acc: %f, sentenceacc: %f' % (
-			acc, word_acc_count / word_count, sent_acc_count / len(test_tags)))
-			sent_acc = sent_acc_count / len(test_tags)
+			sent_acc = self.valid(test_dset)
 			if (sent_acc > maxacc):
-				saver.save(sess, './model/model0.ckpt')
+				# saver.save(sess, './model/model0.ckpt')
 				maxacc = sent_acc
 		logging.info('max acc: %f' % maxacc)
 
@@ -361,11 +310,22 @@ def crossvalid(hidden = 400, keep_prob = 0.8, num_layers = 1, lr = 0.05, max_gra
 		word_dict = pickle.load(f)
 	with open('./data/embeddings.pickle', 'rb') as f:
 		embeddings = pickle.load(f)
-	train_dset = readfile_pos_ner('./data/train.phraselabel.nn.p', word_dict)
-	test_dset = readfile_pos_ner('./data/test.phraselabel.nn.p', word_dict)
+	dset = readfile_pos_ner('./data/train.phraselabel.nn.p', word_dict) \
+		   + readfile_pos_ner('./data/test.phraselabel.nn.p', word_dict)
+	np.random.shuffle(dset)
+	numfold = math.floor(len(dset) / 5)
+	for i in range(0, 5):
+		logging.info('Fold %d:' % i)
+		test_dset = dset[numfold * i: numfold * (i + 1)]
+		train_dset = dset[:numfold * i] + dset[numfold * (i + 1):]
+		model = Model(hidden, num_layers, embeddings, keep_prob, lr, max_grad_norm)
+		model.train(train_dset, test_dset, max_epoch)
+		logging.info('......')
+		logging.info('\n\n')
 
 
 if __name__ == '__main__':
 	logging.basicConfig(filename='./log.txt', filemode='w', level=logging.DEBUG,
 							format='%(asctime)s %(message)s', datefmt='%m-%d %H:%M')
-	main()
+	# main()
+	crossvalid()
